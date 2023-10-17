8a9dea8e35b93404e23e04f9e01468581fd33878
Jayadityan Sethuraman <124813137+JishnuSethuraman@users.noreply.github.com>
2023-10-17 15:19:05 -0400

    temporary brainstorm changes

--- covmatPCA.py
+++ covmatPCA.py
@@ -3,6 +3,7 @@ import numpy as np
 import flasksocketserver
 import json
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 sensor_data = {
     "accelerometer": [],
@@ -31,6 +32,10 @@ for index, row in datacsv.iterrows():
             values = list(payload['values'].values())
             sensor_data[sensor_type].append(values)
 
+for sensor_name, data in sensor_data.items():
+    if len(data) > 1:
+        cov_matrix = np.cov(data, rowvar=False)
+        print(f"Covariance matrix for {sensor_name}:\n", cov_matrix)
 
 WINDOW_SIZE = 10
 
@@ -40,47 +45,35 @@ def create_windows(data, window_size): #create fixed windows from time series
 windowed_sensor_data = {sensor: create_windows(data, WINDOW_SIZE) for sensor, data in sensor_data.items()} #applying the function on sensor data
 
 
-for sensor, windows in windowed_sensor_data.items():
-    if not windows:  # If no windowed data available for a sensor
-        print(f"Empty sensor data, skipping PCA for {sensor}")
-        continue
+def plot_pca(sensor_name, data, windows):
+    if not data or len(data[0]) <= 1:  # Check if data is empty or not 2D
+        return  # If so, skip the plotting for this sensor
     
-    for window in windows:
-        data_array = np.array(window).T
-        if data_array.ndim == 1: #if data_array has 1 dimension
-            data_centered = data_array - np.mean(data_array)
-        else:
-            data_centered = data_array - np.mean(data_array, axis=1, keepdims=True)
-        if data_centered.size > 0:
-            if np.unique(data_centered).size > 1:
-                cov_matrix = np.cov(data_centered)
+    pca = PCA(n_components=2)
+    transformed_data = pca.fit_transform(data)
 
-                eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+    # Get the first window of this sensor
+    first_window = windows[sensor_name][0]
 
-                eigenvalues = eigenvalues[::-1] #reverse order
+    # Get the PCA transformation of the first window
+    first_window_transformed = pca.transform(first_window)
 
-                eigenvectors = eigenvectors[:, ::-1] #reverse column order
+    print(first_window_transformed)
+    
+    # Retrieve the first two principal components of the first window
+    pc1, pc2 = first_window_transformed[0]
 
-                variance = eigenvalues / np.sum(eigenvalues)
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.scatter(transformed_data[:, 0], transformed_data[:, 1], alpha=0.5, label="All data")
+    plt.arrow(0, 0, pc1, pc2, head_width=0.5, head_length=0.5, fc='black', ec='black', label="1st window PCA")
+    plt.title(f"2D PCA of {sensor_name} Data")
+    plt.xlabel("Principal Component 1")
+    plt.ylabel("Principal Component 2")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
-                plt.bar(range(len(eigenvalues)), variance)
-                plt.xlabel('Principal Component')
-                plt.ylabel('Variance')
-                plt.show()
-
-                projected_data = np.dot(data_centered.T, eigenvectors[:, :2])
-
-                scale_factor = 0.7  #to scale the length of the arrow
-                for i in range(2):  #since we're considering 2 principal components in 2D
-                    plt.arrow(0, 0, 
-                            eigenvectors[i, 0]*eigenvalues[i]*scale_factor, 
-                            eigenvectors[i, 1]*eigenvalues[i]*scale_factor,
-                            head_width=1, head_length=1, fc='black', ec='black')
-
-                plt.scatter(projected_data[:, 0], projected_data[:, 1], alpha=0.5)
-                plt.xlabel('Unit 1')
-                plt.ylabel('Unit 2')
-                plt.title('2D PCA from Sensor Data')
-                plt.show()
-            else:
-                print(f"Sensor data has no variance, skipping PCA for {sensor}")
+# Plot PCA for each sensor
+for sensor_name in windowed_sensor_data:
+    plot_pca(sensor_name, sensor_data[sensor_name], windowed_sensor_data)
